ce337aaa896fd3e4546bd7b934ce2e5056957bfe
Devansh <devanshdhopte@gmail.com>
2025-07-09 23:38:24 +0530

    added OpenAI

--- src/main/java/com/example/safeher/controller/ChatBotController.java
+++ src/main/java/com/example/safeher/controller/ChatBotController.java
@@ -1,15 +1,24 @@
+package com.example.safeher.controller;
+
 import com.example.safeher.model.ChatRequest;
 import com.example.safeher.model.ChatResponse;
 import com.example.safeher.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@Autowired
-private ChatService chatService;
+@RestController
+@CrossOrigin(origins = "*")
+public class ChatBotController {
 
-@PostMapping("/chatbot")
-public ChatResponse handleChat(@RequestBody ChatRequest request) {
-    String response = chatService.askGPT(request.getMessage());
-    return new ChatResponse(response);
+    @Autowired
+    private ChatService chatService;
+
+    @PostMapping("/chatbot")
+    public ChatResponse handleChat(@RequestBody ChatRequest request) {
+        String response = chatService.askGPT(request.getMessage());
+        return new ChatResponse(response);
+    }
 }
